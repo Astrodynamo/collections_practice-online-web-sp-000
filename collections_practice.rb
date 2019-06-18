7e24efcd77e93ab2e2dd37e97e3ec73231d6dddb
Amanda King <amapickell@gmail.com>
2019-06-18 18:46:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,9 +41,5 @@ def sum_array (array)
 end
 
 def add_s (array)
-  array.each_with_index.collect do |word, index|
-    if index != 1
-      word << "s"
-    end
-  end
+  array.each_with_index.collect {|word, index| word << "s" if index != 1}
 end
